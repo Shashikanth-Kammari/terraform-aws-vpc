d942b8718305a470152897816d7cdd288d276e79
Shashi <shashikanth0312@gmail.com>
2026-07-25 20:52:48 +0530

    adding

--- expense-terraform-dev/03-bastion/data.tf
+++ expense-terraform-dev/03-bastion/data.tf
@@ -1,3 +1,27 @@
 data "aws_ssm_parameter" "bastion_sg_id" {
   name = "/${var.project_name}/${var.environment}/bastion_sg_id"
 }
+
+
+data "aws_ssm_parameter" "public_subnet_id" {
+  name = "/${var.project_name}/${var.environment}/public_sg_id"
+}
+
+
+data "aws_ami" "ami_info" {
+  most_recent = true
+  owners = ["620549678005"]
+  filter {
+    name = "name"
+    values = ["Devops"]
+  }
+  filter {
+    name = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
--- expense-terraform-dev/03-bastion/main.tf
+++ expense-terraform-dev/03-bastion/main.tf
@@ -1,14 +1,19 @@
 module "bastion" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "single-instance"
+  name = "${var.project_name}-${var.environment}-bastion"
 
   instance_type = "t3.micro"
   vpc_security_group_ids = [data.aws_ssm_parameter.bastion_sg_id.value]
-  subnet_id     = 
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+  # converting string list to list and get first elementelement(split("," , data.aws_ssm_parameter.public_subnet_id.value), 0)
+
+  subnet_id     = local.public_subnet_id
+  ami = data.aws_ami.ami_info.id
+  tags = merge(
+    var.common_tags,
+    {
+        Name = "${var.project_name}-${var.environment}-bastion"
+    }
+  )
 }
