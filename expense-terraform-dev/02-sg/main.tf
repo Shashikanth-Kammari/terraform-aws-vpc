829da3e5abcdd1e1dc672691aba2d7434c297500
Shashi <shashikanth0312@gmail.com>
2026-07-25 13:44:37 +0530

    adding

--- expense-terraform-dev/02-sg/main.tf
+++ expense-terraform-dev/02-sg/main.tf
@@ -38,3 +38,23 @@ resource "aws_security_group_rule" "db_backend" {
   source_security_group_id = module.backend.sg_id  #source where you are getting trafic i.e backend sg id
   security_group_id = module.db.sg_id
 }
+
+# Back end accepting form frontend
+resource "aws_security_group_rule" "backend_frontend" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = module.frontend.sg_id  #source where you are getting trafic i.e frontend sg id
+  security_group_id = module.backend.sg_id
+}
+
+# Back end accepting form frontend
+resource "aws_security_group_rule" "frontend_public" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.frontend.sg_id
+}
