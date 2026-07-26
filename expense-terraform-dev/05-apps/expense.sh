24f00328bcad11d184dd153d67c0b713cca69ea2
Shashi <shashikanth0312@gmail.com>
2026-07-26 19:17:37 +0530

    adding

--- expense-terraform-dev/05-apps/expense.sh
+++ expense-terraform-dev/05-apps/expense.sh
@@ -3,4 +3,5 @@ dnf install ansible -y
 cd /temp
 git clone https://github.com/Shashikanth-Kammari/expense-ansible-roles.git
 cd expense-ansible-roles
-ansible-playbook main.yaml -e component=backend
+ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
+ansible-playbook main.yaml -e component=frontend 
