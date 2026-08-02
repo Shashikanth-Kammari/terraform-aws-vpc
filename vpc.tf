03e25e6cc160a3dd7786a08a8bef2c6780b9ba8c
Shashikanth Kammari <shashikanth0312@gmail.com>
2026-08-02 23:40:15 +0530

    adding

--- vpc.tf
+++ vpc.tf
@@ -148,7 +148,7 @@ resource "aws_route_table" "database" {
 }
 
 
-#### Routes ####
+#### Routes for public and private and database subnets ####
 resource "aws_route" "public_route" {
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
